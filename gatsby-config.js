91f6045ba13252a4b6ce1d852392f3e97725c373
Matthew Davidson <mad1512@optusnet.com.au>
2020-05-27 23:41:16 +1000

    Updated site description.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteMetadata: {
         title: `Recompiled`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+        description: `A blog and resource curation for all things Software Engineering.`,
         author: `@mattdavo`,
     },
     plugins: [
