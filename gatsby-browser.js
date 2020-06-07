f703e39da537098147ca4f64216ca13616d72eee
Matthew Davidson <mad1512@optusnet.com.au>
2020-06-07 12:32:56 +1000

    Added c++ syntax highlighting.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,10 +10,17 @@ import PageElement from './gatsby/wrapPageElement';
 import RootElement from './gatsby/wrapRootElement';
 
 import Prism from 'prism-react-renderer/prism';
+import refractor from 'refractor';
 import swiftLang from 'refractor/lang/swift';
 import rubyLang from 'refractor/lang/ruby';
-swiftLang(Prism);
-rubyLang(Prism);
+import cppLang from 'refractor/lang/cpp';
+
+// More robust way of seeting language instead of cppLang(Prism)
+// See: https://github.com/wooorm/refractor/issues/27#issuecomment-609888676
+Object.setPrototypeOf(refractor, Prism);
+refractor.register(cppLang);
+refractor.register(rubyLang);
+refractor.register(swiftLang);
 
 export const wrapPageElement = PageElement;
 
